624b49a3527353e94bf8b13f243aa28ba70ac85a
DIVYA <lokwanidivya768@gmail.com>
2026-08-03 21:50:25 +0530

    update

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,8 +3,8 @@ import os
 
 load_dotenv()
 
-APP_NAME = os.getenv("APP_NAME")
-APP_VERSION = os.getenv("APP_VERSION")
+APP_NAME = os.getenv("APP_NAME", "Smart Retail AI")
+APP_VERSION = os.getenv("APP_VERSION", "1.0.0")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 API_KEY = os.getenv("API_KEY")
